f722bd512e1a36ddc6a2f583595ed32205038ebf
vtlam1793 <141825182+vtlam1793@users.noreply.github.com>
2023-09-22 16:55:15 +0700

    update form day3

---
+++ homework/day3.js
@@ -0,0 +1,177 @@
+// =========================================================
+/*
+    BAI TAP 1
+    INPUT   : Nhap vao n
+    PROCESS : In so do ra 10 lan
+    OUTPUT  : undefined
+*/  
+function bai_tap_1(n){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 2
+    INPUT   : 
+    PROCESS : In so do ra 1 -> 100
+    OUTPUT  : 
+*/  
+function bai_tap_2(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 3
+    INPUT   : 
+    PROCESS : In so do ra 100 -> 1
+    OUTPUT  : 
+*/  
+function bai_tap_3(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 4
+    INPUT   : 
+    PROCESS : In so do ra 1 -> 100 mà chia hết cho 3
+    OUTPUT  : 
+*/  
+function bai_tap_4(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 5
+    INPUT   : 
+    PROCESS : In so do ra 1 -> 100 mà chia hết cho 7
+    OUTPUT  : 
+*/  
+function bai_tap_5(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 6
+    INPUT   : 
+    PROCESS : Tính tổng của 1,2,3,4....5000
+    OUTPUT  : 
+*/  
+function bai_tap_6(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 7
+    INPUT   : 
+    PROCESS : Tính tổng của 5001,5002,...,10000
+    OUTPUT  : 
+*/  
+function bai_tap_7(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 8
+    INPUT   : 
+    PROCESS : Tính tổng của 30 số 5, 10, 15, 20, 25
+    OUTPUT  : 
+*/  
+function bai_tap_8(){
+    /* 
+        PROCESS
+    */
+
+    
+    return;
+}
+
+// =========================================================
+/*
+    BAI TAP 9
+    INPUT   : Số n
+    PROCESS : Tính giai thừa
+    OUTPUT  : n!
+*/  
+function bai_tap_9(){
+    var ret = 1;
+    /* 
+        PROCESS
+    */
+    
+    return ret;
+}
+
+// =========================================================
+/*
+    BAI TAP 10
+    INPUT   : Số n
+    PROCESS : Kiểm tra số này phải số nguyên tố không?
+    OUTPUT  : 
+        * true  - số nguyên tố
+        * false - không phải số nguyên tố
+*/  
+function bai_tap_10(){
+    var ret = false;
+    /* 
+        PROCESS
+    */
+    
+    return ret;
+}
+
+// =========================================================
+/*
+    BAI TAP 11
+    INPUT   : Số n
+    PROCESS : In ra n số fibnacci đầu tiên
+    OUTPUT  : 
+*/  
+function bai_tap_11(n){
+    /* 
+        PROCESS
+    */
+    
+
+}
+
--- test.py
+++
@@ -1,8 +0,0 @@
-tb=0
-if (tb > 9):
-    print("A")
-    print("B")
-else:
-    print("A")
-
-print("C")
