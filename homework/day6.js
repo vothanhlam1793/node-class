d509450fc0bcda502c6650c70cd019f57e389350
Tal Tal <vothanhlam1793@gmail.com>
2023-09-28 19:58:22 +0700

    update hw 6

---
+++ homework/day6.js
@@ -0,0 +1,20 @@
+function bai_tap_19(arr){
+    // Sap xep chuoi so theo thu tu tu lon den be
+    // Vi du: [6,5,3,9,3]
+    // Ket qua: [9,6,5,3,3]
+
+}
+
+function bai_tap_20(arr){
+    // Sap xep chuoi so theo thu tu tu be den lon
+    // Vi du: [6,5,3,9,3]
+    // Ket qua: [3, 3, 5, 6, 9]
+
+}
+
+function bai_tap_21(arr){
+    // Loc cac so chan trong arr
+    // Vi du: [6,5,3,9,3]
+    // Ket qua: [6]
+    
+}
