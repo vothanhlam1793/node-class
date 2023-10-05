c0706a8db41ee73f40f4c5a48a16adf50b1ec5a9
Tal Tal <vothanhlam1793@gmail.com>
2023-10-05 21:46:52 +0700

    push

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 var count = 0;
 function countup(){
+
+    // Selector
     var p = document.getElementById('demo');
     p.innerHTML = Date();
+    console.log(count++);
 }
 
+function countup1(){
+
+    // Selector
+    var p = document.getElementById('demo');
+    p.innerHTML = Date();
+    console.log("COUL " + count++);
+}
 function hello(){
     console.log("Hello World");
 }
 
 setInterval(countup, 1000);
+setInterval(countup1, 500);
+console.log("Xin chao nhé!");
