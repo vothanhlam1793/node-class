2e165ffc35e00a23a9ded2f90a45719550312d94
Tal Tal <vothanhlam1793@gmail.com>
2023-10-05 21:01:29 +0700

    up load B8

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("Welcome to K18 NODEJS");
+function hello(){
+    console.log("Hello World");
+
+    // Selector 
+    document.get
+    var p = document.getElementById('demo');
+
+    p.innerHTML = Date();
+    // document.getElementById('demo').innerHTML = Date();
+}
