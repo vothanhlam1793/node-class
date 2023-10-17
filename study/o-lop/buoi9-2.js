844e05f47955170f0107a9540863fc1c14d8f2b6
Tal Tal <vothanhlam1793@gmail.com>
2023-10-17 19:47:11 +0700

    emit/on

---
+++ study/o-lop/buoi9-2.js
@@ -0,0 +1,27 @@
+// EMIT - ON
+// EVENT
+
+var events = require('events'); // Thu vien
+
+var eventEmitter = new events.EventEmitter();
+
+var i = 0;
+function helloworld(){
+    console.log("Hello World " + (i++));
+    eventEmitter.emit("lam");
+}
+function black(){
+    console.log("BLACK");
+    if(i > 20){
+        clearInterval(iX);
+    }
+}
+
+eventEmitter.on('hello', helloworld);
+
+eventEmitter.on('lam', black);
+
+var iX = setInterval(function(){
+    eventEmitter.emit("hello");
+    // eventEmitter.emit("lam");
+}, 200);
