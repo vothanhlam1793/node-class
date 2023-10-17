2778c35a6c06c0f4faa6e63fe2a371e951be51b2
Tal Tal <vothanhlam1793@gmail.com>
2023-10-17 19:38:24 +0700

    update set clear

--- study/o-lop/buoi9.js
+++ study/o-lop/buoi9.js
@@ -1,5 +1,8 @@
 function add(x, y, cb){
+    console.log(typeof cb);
+
     cb(); // function -> run
+    
     return x + y;
 }
 
@@ -7,7 +10,54 @@ function phepcong(){
     console.log("ADD");
 }
 
+function pheptru(){
+    console.log("SUB");
+}
 
-c = add(9,8, phepcong );
+// c = add(9,8, pheptru );
 
-console.log(c);
+function ham1giay(){
+    console.log("SAU 1 GIAU");
+}
+
+
+console.log("START");
+
+// setTimeout(ham1giay, 1000);
+// setTimeout(ham1giay, 3000);
+console.log("END");
+
+// setTimeout(, 60*1000*60);
+var i = 0;
+var iX = setInterval(function(){
+    if(i > 10){
+         i = 0;
+         clearTimeout(tX);
+    } else {
+        // Van chay else 
+    }
+    console.log(i++);
+    
+}, 200);
+
+var tX = setTimeout(function(){
+    console.log("xoá hệ thống - 1 ");
+    clearInterval(iX);
+    clearTimeout(tY);
+}, 1*1000);
+
+function yy(){
+    console.log("xoá hệ thống - 2");
+    clearInterval(iX);  
+}
+var tY = setTimeout(yy, 5*1000);
+
+clearTimeout(tX);
+
+/*
+0 ------- 1 ------ 2 ------- 3 -------- 4 -------- 5 ------
+iX ------------------------------------------------ X
+tX --------------- X
+tY ------------------------------------------------ iX (X)
+
+*/ 
