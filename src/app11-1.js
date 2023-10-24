f5088c959eb0237bd3bd4d3f0555941ad372658e
Tal Tal <vothanhlam1793@gmail.com>
2023-10-24 20:36:21 +0700

    giai phuong trinh bac 2

--- src/app11-1.js
+++ src/app11-1.js
@@ -12,7 +12,8 @@ function changeInput(){
 function btnPhepToan(pt){
     var i1 = document.getElementById('inp1');
     var i2 = document.getElementById('inp2');
-    var kq = document.getElementById('kq');
+
+    
     var x = parseInt(i1.value);
     var y = parseInt(i2.value);
 
@@ -35,4 +36,58 @@ function btnPhepToan(pt){
         kq.innerHTML = x / y;
         return;
     }
+}
+
+
+function giaiphuongtrinh(){
+    // INPUT
+    var iA = document.getElementById('inpA');
+    var iB = document.getElementById('inpB');
+    var iC = document.getElementById('inpC');
+    
+    // OUTPUT
+    var kq = document.getElementById('kq');
+
+    // CHUYEN CHUOI THANH SO
+    var a = parseInt(iA.value);
+    var b = parseInt(iB.value);
+    var c = parseInt(iC.value);
+
+    var ret = ""; // KHAI BAO BIEN LAN DAU
+
+    if(a == 0){
+        // PTB1: Bx + C = 0
+        if(b == 0){
+            ret = "Phương trình vô nghiệm";
+        } else {
+            ret = "Phương trình bậc 1 có nghiệm X = " + (-c / b);
+        }
+
+    } else {
+        // PTB2
+        var denta = b*b - 4*a*c;
+
+        if(denta < 0){
+            ret = "Phương trình vô nghiệm";
+        } else {
+
+        }
+
+        if(denta==0){
+            ret = "Phương trình có nghiệm kép: X1 = X2 = " + (-b/(2*a));
+        } else {
+
+        }
+
+        if(denta > 0){
+            var x1 = ((-b + Math.sqrt(denta))/(2*a));
+            var x2 = ((-b - Math.sqrt(denta))/(2*a));
+            ret = "Phương trình có 2 nghiệm: <br>X1 = " + x1 + "<br>X2 = " + x2;
+        } else {
+
+        }
+
+    }
+
+    kq.innerHTML = ret;
 }
