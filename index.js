75de825d4a58e41f330286cf7f8ac74dfe8ebd77
Tal Tal <vothanhlam1793@gmail.com>
2023-12-05 20:57:07 +0700

    note pad

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 var path = require('path');
+const bodyParser = require('body-parser');
+
+var fs = require("fs");
 
 
 const port = 3000
 
 // File
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Views
 app.set('views', path.join(__dirname, 'views'));
@@ -15,13 +19,33 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     console.log("REQ: ", req.query);
+    fs.readFile(__dirname + "/public/luutru.txt", function(e, d){
+      var data = "";
+      if(e){
+        data = "ERR";
+      } else {
+        data = d;
+      }
 
-    res.render('index', {
-        counter: parseInt(req.query.counter),
-        name: req.query.name
-    }); 
+      res.render('index', {
+          counter: parseInt(req.query.counter),
+          name: req.query.name,
+          data: data
+      }); 
+    })
 });
 
+
+app.post("/luutru", function(req, res){
+  console.log(req.body);
+  fs.writeFile(__dirname + "/public/luutru.txt", req.body.data, function(e){
+    if(e){
+      res.send("ERR: " + e);
+    } else {
+      res.send("OK");
+    }
+  })
+});
 app.listen(port, function(){
   console.log(`Example app listening on port ${port}`)
 })
