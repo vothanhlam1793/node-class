7098b296ea3fc87d421e83425a2fa0ef4a53d82f
Tal Tal <vothanhlam1793@gmail.com>
2023-12-07 21:27:13 +0700

    ts2

--- index.js
+++ index.js
@@ -35,6 +35,24 @@ app.get('/', function(req, res){
     })
 });
 
+app.get("/ts2", function(req, res){
+  fs.readFile(__dirname + "/public/luutru2.txt", function(e, d){
+    var data = "";
+    if(e){
+      data = "ERR";
+    } else {
+      data = d;
+    }
+    var str = data.toString();
+    var arr = str.split("\n");
+
+    console.log(arr);
+
+    res.render('ts2', {
+        data: arr
+    }); 
+  })
+});
 
 app.post("/luutru", function(req, res){
   console.log(req.body);
@@ -46,6 +64,54 @@ app.post("/luutru", function(req, res){
     }
   })
 });
+
+app.post("/luutru2", function(req, res){
+  console.log(req.body);
+  fs.appendFile(__dirname + "/public/luutru2.txt", "\n" + req.body.data, function(e){
+    if(e){
+      res.send("ERR: " + e);
+    } else {
+      // res.send("OK");
+      res.redirect("/ts2");
+    }
+  });
+});
+
+app.get("/xoa", function(req, res){
+  // ?vitri=?
+
+  // B1 - Doc file 
+  // B2 - Chuyen thanh arr
+  // B3 - Xoa du lieu o vi arr (vitri)
+  // B4 - Save moi file writeFile
+  // B5 - OK/ERR
+
+  // res.send("Bam nut xoa: " + req.query.vitri);
+  // Buoc 1
+  fs.readFile(__dirname + "/public/luutru2.txt", function(err, data){
+    if(err){
+      res.send("ERR: " + err);
+    } else {
+      // Buoc 2
+      var str = data.toString();
+      var arr = str.split("\n");
+
+      console.log("TRUOC", arr);
+      arr.splice( req.query.vitri, 1);
+      console.log("SAU", arr);
+
+      var str2 = arr.join("\n");
+      fs.writeFile(__dirname + "/public/luutru2.txt", str2, function(e){
+        if(e){
+          res.send("ERR: " + e);
+        } else {
+          res.redirect("/ts2");
+        }
+      })
+    }
+  });
+});
+
 app.listen(port, function(){
   console.log(`Example app listening on port ${port}`)
 })
